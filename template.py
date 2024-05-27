ebffc4658e22e736f3f723518597b046f9f207e6
KR-16 <keerthirajkv2@gmail.com>
2024-05-27 01:36:10 -0500

    updated requirements.txt, created templates folder

--- template.py
+++ template.py
@@ -19,6 +19,7 @@ list_of_files = [
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
+    f"templates/index.html",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
